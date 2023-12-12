1d4a5d519cdd1f63cdf7da09aa8f15bf8dd99bef
light <3535888142@qq.com>
2023-12-12 14:47:35 +0800

    nvim updated.

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -50,3 +50,16 @@ keymap.set("n", "<C-H>", ":bprevious<CR>")
 ---- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 keymap.set("n", "<F5>", ":!g++ main.cpp -o main && ./main < input.txt > output.txt<CR>")
+
+keymap.set("n", "<leader>tp", [[:lua loadTemplate()<CR>]], { noremap = true, silent = true })
+
+function loadTemplate()
+    local template_path = vim.fn.expand("~/.config/nvim/template.cpp")
+    if vim.fn.filereadable(template_path) == 1 then
+        local template_content = vim.fn.readfile(template_path)
+        vim.fn.append(0, template_content)
+        print("Template loaded.")
+    else
+        print("Loading failed.")
+    end
+end
